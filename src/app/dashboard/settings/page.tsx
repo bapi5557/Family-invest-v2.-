97c0f2211563dc46455836db0f09db61b4d98a23
Bapi Maharana <anchalikamahabidyalaya123@gmail.com>
2026-06-04 11:46:18 +0000

    Try fixing this error: `Build Error: Ecmascript file had an error. Error

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { useState, useEffect, useMemo } from "react";
@@ -307,10 +306,4 @@ export default function SettingsPage() {
       </div>
     </div>
   );
-}
-
-function Plus(props: any) {
-  return (
-    <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
-  )
 }
